88452574a2eeb8dcf1c6dc96c6db92fc8bb3acdc
Hantar0 <Hantar0@users.noreply.github.com>
2015-10-26 11:27:08 +0100

    failed

--- bankonet-conseiller/src/main/java/com/bankonet/presentation/Launcher.java
+++ bankonet-conseiller/src/main/java/com/bankonet/presentation/Launcher.java
@@ -3,7 +3,7 @@ package com.bankonet.presentation;
 public class Launcher {
 
 	public static void main(String[] args) throws Exception {
-
+		#yolo
 		ConseillerApp app = new ConseillerApp();
 		app.start(args[0]);
 	
